540c8cf0563564117491661abf84bb27a44a5bc4
xueshuming <xueshuming@minigui.org>
2021-11-10 11:55:13 +0800

    add fetcher-http-header-name.h

---
+++ Source/capi/capi/fetcher-http-header-name.h
@@ -0,0 +1,118 @@
+/*
+ * @file fetcher-http-header-name.h
+ * @author XueShuming
+ * @date 2021/11/07
+ * @brief The interfaces for fetcher message.
+ *
+ * Copyright (C) 2021 FMSoft <https://www.fmsoft.cn>
+ *
+ * This file is a part of PurC (short for Purring Cat), an HVML interpreter.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PURC_FETCHER_HTTP_HEADER_NAME_H
+#define PURC_FETCHER_HTTP_HEADER_NAME_H
+
+enum pcfetcher_http_header_name {
+    Accept,
+    AcceptCharset,
+    AcceptEncoding,
+    AcceptLanguage,
+    AcceptRanges,
+    AccessControlAllowCredentials,
+    AccessControlAllowHeaders,
+    AccessControlAllowMethods,
+    AccessControlAllowOrigin,
+    AccessControlExposeHeaders,
+    AccessControlMaxAge,
+    AccessControlRequestHeaders,
+    AccessControlRequestMethod,
+    Age,
+    Authorization,
+    CacheControl,
+    Connection,
+    ContentDisposition,
+    ContentEncoding,
+    ContentLanguage,
+    ContentLength,
+    ContentLocation,
+    ContentRange,
+    ContentSecurityPolicy,
+    ContentSecurityPolicyReportOnly,
+    ContentType,
+    Cookie,
+    Cookie2,
+    CrossOriginResourcePolicy,
+    DNT,
+    Date,
+    DefaultStyle,
+    ETag,
+    Expect,
+    Expires,
+    Host,
+    IcyMetaInt,
+    IcyMetadata,
+    IfMatch,
+    IfModifiedSince,
+    IfNoneMatch,
+    IfRange,
+    IfUnmodifiedSince,
+    KeepAlive,
+    LastEventID,
+    LastModified,
+    Link,
+    Location,
+    Origin,
+    PingFrom,
+    PingTo,
+    Pragma,
+    ProxyAuthorization,
+    Purpose,
+    Range,
+    Referer,
+    ReferrerPolicy,
+    Refresh,
+    SecWebSocketAccept,
+    SecWebSocketExtensions,
+    SecWebSocketKey,
+    SecWebSocketProtocol,
+    SecWebSocketVersion,
+    ServerTiming,
+    ServiceWorker,
+    ServiceWorkerAllowed,
+    SetCookie,
+    SetCookie2,
+    SourceMap,
+    TE,
+    TimingAllowOrigin,
+    Trailer,
+    TransferEncoding,
+    Upgrade,
+    UpgradeInsecureRequests,
+    UserAgent,
+    Vary,
+    Via,
+    XContentTypeOptions,
+    XDNSPrefetchControl,
+    XFrameOptions,
+    XSourceMap,
+    XTempTablet,
+    XXSSProtection,
+};
+
+
+#endif /* not defined PURC_FETCHER_HTTP_HEADER_NAME_H */
+
+

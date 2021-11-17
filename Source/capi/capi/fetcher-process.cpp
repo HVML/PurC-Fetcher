9e633351409347e8d7aba93acb2cdc624a11c00c
xueshuming <xueshuming@minigui.org>
2021-11-17 19:56:53 +0800

    update fetcher-process

--- Source/capi/capi/fetcher-process.cpp
+++ Source/capi/capi/fetcher-process.cpp
@@ -14,10 +14,7 @@
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY
-{
-}
- without even the implied warranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
@@ -26,28 +23,54 @@
  */
 
 #include "config.h"
-
 #include "fetcher-process.h"
 
-#include "ProcessLauncher.h"
-
-#if ENABLE(LINK_PURC_FETCHER)
+#include <wtf/RunLoop.h>
 
 using namespace PurCFetcher;
 
-PcFetcherProcess::PcFetcherProcess(struct pcfetcher* fetcher)
-    : m_fetcher(fetcher)
+PcFetcherProcess::PcFetcherProcess(struct pcfetcher* fetcher,
+        bool alwaysRunsAtBackgroundPriority)
+ : m_fetcher(fetcher)
+ , m_alwaysRunsAtBackgroundPriority(alwaysRunsAtBackgroundPriority)
 {
 }
 
 PcFetcherProcess::~PcFetcherProcess()
 {
+    if (m_connection)
+        m_connection->invalidate();
+
+    if (m_processLauncher) {
+        m_processLauncher->invalidate();
+        m_processLauncher = nullptr;
+    }
+    auto pendingMessages = WTFMove(m_pendingMessages);
+    for (auto& pendingMessage : pendingMessages) {
+        if (pendingMessage.asyncReplyInfo)
+            pendingMessage.asyncReplyInfo->first(nullptr);
+    }
+}
+
+void PcFetcherProcess::getLaunchOptions(ProcessLauncher::LaunchOptions& launchOptions)
+{
+    launchOptions.processIdentifier = m_processIdentifier;
+
+    if (const char* userDirectorySuffix = getenv("DIRHELPER_USER_DIR_SUFFIX"))
+        launchOptions.extraInitializationData.add("user-directory-suffix"_s, userDirectorySuffix);
+
+    if (m_alwaysRunsAtBackgroundPriority)
+        launchOptions.extraInitializationData.add("always-runs-at-background-priority"_s, "true");
+
+    launchOptions.processType = ProcessLauncher::ProcessType::Network;
+
 }
 
 void PcFetcherProcess::connect()
 {
+    ASSERT(!m_processLauncher);
     ProcessLauncher::LaunchOptions launchOptions;
-    launchOptions.processType = ProcessLauncher::ProcessType::Network;
+    getLaunchOptions(launchOptions);
     m_processLauncher = ProcessLauncher::create(this, WTFMove(launchOptions));
 }
 
@@ -57,11 +80,166 @@ void PcFetcherProcess::terminate()
         m_processLauncher->terminateProcess();
 }
 
-void PcFetcherProcess::didFinishLaunching(ProcessLauncher*,
-            IPC::Connection::Identifier identifier)
+PcFetcherProcess::State PcFetcherProcess::state() const
 {
-    this->m_identifier = identifier;
+    if (m_processLauncher && m_processLauncher->isLaunching())
+        return PcFetcherProcess::State::Launching;
+
+    if (!m_connection)
+        return PcFetcherProcess::State::Terminated;
+
+    return PcFetcherProcess::State::Running;
 }
 
+bool PcFetcherProcess::wasTerminated() const
+{
+    switch (state()) {
+    case PcFetcherProcess::State::Launching:
+        return false;
+    case PcFetcherProcess::State::Terminated:
+        return true;
+    case PcFetcherProcess::State::Running:
+        break;
+    }
 
-#endif // ENABLE(LINK_PURC_FETCHER)
+    auto pid = processIdentifier();
+    if (!pid)
+        return true;
+
+    return false;
+}
+
+bool PcFetcherProcess::sendMessage(std::unique_ptr<IPC::Encoder> encoder, OptionSet<IPC::SendOption> sendOptions, Optional<std::pair<CompletionHandler<void(IPC::Decoder*)>, uint64_t>>&& asyncReplyInfo, ShouldStartProcessThrottlerActivity shouldStartProcessThrottlerActivity)
+{
+    if (asyncReplyInfo && canSendMessage() && shouldStartProcessThrottlerActivity == ShouldStartProcessThrottlerActivity::Yes) {
+        auto completionHandler = std::exchange(asyncReplyInfo->first, nullptr);
+        asyncReplyInfo->first = [completionHandler = WTFMove(completionHandler)](IPC::Decoder* decoder) mutable {
+            completionHandler(decoder);
+        };
+    }
+
+    switch (state()) {
+    case State::Launching:
+        // If we're waiting for the child process to launch, we need to stash away the messages so we can send them once we have a connection.
+        m_pendingMessages.append({ WTFMove(encoder), sendOptions, WTFMove(asyncReplyInfo) });
+        return true;
+
+    case State::Running:
+        if (asyncReplyInfo)
+            IPC::addAsyncReplyHandler(*connection(), asyncReplyInfo->second, std::exchange(asyncReplyInfo->first, nullptr));
+        if (connection()->sendMessage(WTFMove(encoder), sendOptions))
+            return true;
+        break;
+
+    case State::Terminated:
+        break;
+    }
+
+    if (asyncReplyInfo && asyncReplyInfo->first) {
+        RunLoop::current().dispatch([completionHandler = WTFMove(asyncReplyInfo->first)]() mutable {
+            completionHandler(nullptr);
+        });
+    }
+
+    return false;
+}
+
+void PcFetcherProcess::addMessageReceiver(IPC::ReceiverName messageReceiverName, IPC::MessageReceiver& messageReceiver)
+{
+    m_messageReceiverMap.addMessageReceiver(messageReceiverName, messageReceiver);
+}
+
+void PcFetcherProcess::addMessageReceiver(IPC::ReceiverName messageReceiverName, uint64_t destinationID, IPC::MessageReceiver& messageReceiver)
+{
+    m_messageReceiverMap.addMessageReceiver(messageReceiverName, destinationID, messageReceiver);
+}
+
+void PcFetcherProcess::removeMessageReceiver(IPC::ReceiverName messageReceiverName, uint64_t destinationID)
+{
+    m_messageReceiverMap.removeMessageReceiver(messageReceiverName, destinationID);
+}
+
+void PcFetcherProcess::removeMessageReceiver(IPC::ReceiverName messageReceiverName)
+{
+    m_messageReceiverMap.removeMessageReceiver(messageReceiverName);
+}
+
+bool PcFetcherProcess::dispatchMessage(IPC::Connection& connection, IPC::Decoder& decoder)
+{
+    return m_messageReceiverMap.dispatchMessage(connection, decoder);
+}
+
+bool PcFetcherProcess::dispatchSyncMessage(IPC::Connection& connection, IPC::Decoder& decoder, std::unique_ptr<IPC::Encoder>& replyEncoder)
+{
+    return m_messageReceiverMap.dispatchSyncMessage(connection, decoder, replyEncoder);
+}
+
+void PcFetcherProcess::didFinishLaunching(ProcessLauncher*, IPC::Connection::Identifier connectionIdentifier)
+{
+    ASSERT(!m_connection);
+
+    if (!IPC::Connection::identifierIsValid(connectionIdentifier))
+        return;
+
+    m_connection = IPC::Connection::createServerConnection(connectionIdentifier, *this);
+
+    connectionWillOpen(*m_connection);
+    m_connection->open();
+
+    for (auto&& pendingMessage : std::exchange(m_pendingMessages, { })) {
+        if (!shouldSendPendingMessage(pendingMessage))
+            continue;
+        auto encoder = WTFMove(pendingMessage.encoder);
+        auto sendOptions = pendingMessage.sendOptions;
+        if (pendingMessage.asyncReplyInfo)
+            IPC::addAsyncReplyHandler(*connection(), pendingMessage.asyncReplyInfo->second, WTFMove(pendingMessage.asyncReplyInfo->first));
+        m_connection->sendMessage(WTFMove(encoder), sendOptions);
+    }
+}
+
+void PcFetcherProcess::shutDownProcess()
+{
+    switch (state()) {
+    case State::Launching:
+        m_processLauncher->invalidate();
+        m_processLauncher = nullptr;
+        break;
+    case State::Running:
+        break;
+    case State::Terminated:
+        return;
+    }
+
+    if (!m_connection)
+        return;
+
+#if 0
+    if (canSendMessage())
+        send(Messages::AuxiliaryProcess::ShutDown(), 0);
+#endif
+
+    m_connection->invalidate();
+    m_connection = nullptr;
+}
+
+void PcFetcherProcess::setProcessSuppressionEnabled(bool processSuppressionEnabled)
+{
+    UNUSED_PARAM(processSuppressionEnabled);
+}
+
+void PcFetcherProcess::didClose(IPC::Connection&)
+{
+}
+
+void PcFetcherProcess::didReceiveInvalidMessage(IPC::Connection&, IPC::MessageName)
+{
+}
+
+void PcFetcherProcess::connectionWillOpen(IPC::Connection&)
+{
+}
+
+void PcFetcherProcess::logInvalidMessage(IPC::Connection&, IPC::MessageName)
+{
+}
+
--- Source/capi/capi/fetcher-process.h
+++ Source/capi/capi/fetcher-process.h
@@ -27,31 +27,174 @@
 
 #if ENABLE(LINK_PURC_FETCHER)
 
-#include "purc/purc.h"
 #include "fetcher-remote.h"
-#include "ProcessLauncher.h"
 
-class PcFetcherProcess :  public PurCFetcher::ProcessLauncher::Client {
+#include "Connection.h"
+#include "MessageReceiverMap.h"
+#include "ProcessLauncher.h"
+#include "ProcessIdentifier.h"
+
+#include <wtf/ProcessID.h>
+#include <wtf/SystemTracing.h>
+#include <wtf/ThreadSafeRefCounted.h>
+
+using namespace PurCFetcher;
+
+class PcFetcherProcess : ProcessLauncher::Client, public IPC::Connection::Client {
+    WTF_MAKE_NONCOPYABLE(PcFetcherProcess);
+
 public:
-    PcFetcherProcess(struct pcfetcher* fetcher);
-    ~PcFetcherProcess();
+    PcFetcherProcess(struct pcfetcher* fetcher,
+            bool alwaysRunsAtBackgroundPriority = false);
+
+    virtual ~PcFetcherProcess();
 
     void connect();
     void terminate();
 
-    void didFinishLaunching(PurCFetcher::ProcessLauncher*,
-            IPC::Connection::Identifier identifier);
+    template<typename T> bool send(T&& message, uint64_t destinationID, OptionSet<IPC::SendOption> sendOptions = { });
+    template<typename T> bool sendSync(T&& message, typename T::Reply&&, uint64_t destinationID, Seconds timeout = 1_s, OptionSet<IPC::SendSyncOption> sendSyncOptions = { });
+
+    enum class ShouldStartProcessThrottlerActivity : bool { No, Yes };
+    template<typename T, typename C> void sendWithAsyncReply(T&&, C&&, uint64_t destinationID = 0, OptionSet<IPC::SendOption> = { }, ShouldStartProcessThrottlerActivity = ShouldStartProcessThrottlerActivity::Yes);
+
+    template<typename T, typename U>
+    bool send(T&& message, ObjectIdentifier<U> destinationID, OptionSet<IPC::SendOption> sendOptions = { })
+    {
+        return send<T>(WTFMove(message), destinationID.toUInt64(), sendOptions);
+    }
+
+    template<typename T, typename U>
+    bool sendSync(T&& message, typename T::Reply&& reply, ObjectIdentifier<U> destinationID, Seconds timeout = 1_s, OptionSet<IPC::SendSyncOption> sendSyncOptions = { })
+    {
+        return sendSync<T>(WTFMove(message), WTFMove(reply), destinationID.toUInt64(), timeout, sendSyncOptions);
+    }
+
+    IPC::Connection* connection() const
+    {
+        ASSERT(m_connection);
+        return m_connection.get();
+    }
+
+    bool hasConnection(const IPC::Connection& connection) const
+    {
+        return m_connection == &connection;
+    }
+
+    void addMessageReceiver(IPC::ReceiverName, IPC::MessageReceiver&);
+    void addMessageReceiver(IPC::ReceiverName, uint64_t destinationID, IPC::MessageReceiver&);
+    void removeMessageReceiver(IPC::ReceiverName, uint64_t destinationID);
+    void removeMessageReceiver(IPC::ReceiverName);
+
+    template <typename T>
+    void addMessageReceiver(IPC::ReceiverName messageReceiverName, ObjectIdentifier<T> destinationID, IPC::MessageReceiver& receiver)
+    {
+        addMessageReceiver(messageReceiverName, destinationID.toUInt64(), receiver);
+    }
+
+    template <typename T>
+    void removeMessageReceiver(IPC::ReceiverName messageReceiverName, ObjectIdentifier<T> destinationID)
+    {
+        removeMessageReceiver(messageReceiverName, destinationID.toUInt64());
+    }
+
+    enum class State {
+        Launching,
+        Running,
+        Terminated,
+    };
+    State state() const;
+    bool isLaunching() const { return state() == State::Launching; }
+    bool wasTerminated() const;
+
+    ProcessID processIdentifier() const { return m_processLauncher ? m_processLauncher->processIdentifier() : 0; }
+
+    bool canSendMessage() const { return state() != State::Terminated;}
+    bool sendMessage(std::unique_ptr<IPC::Encoder>, OptionSet<IPC::SendOption>, Optional<std::pair<CompletionHandler<void(IPC::Decoder*)>, uint64_t>>&& asyncReplyInfo = WTF::nullopt, ShouldStartProcessThrottlerActivity = ShouldStartProcessThrottlerActivity::Yes);
+
+    void shutDownProcess();
+
+    PurCFetcher::ProcessIdentifier coreProcessIdentifier() const { return m_processIdentifier; }
+
+    void setProcessSuppressionEnabled(bool);
+
+protected:
+    // ProcessLauncher::Client
+    void didFinishLaunching(ProcessLauncher*, IPC::Connection::Identifier) override;
+
+    bool dispatchMessage(IPC::Connection&, IPC::Decoder&);
+    bool dispatchSyncMessage(IPC::Connection&, IPC::Decoder&, std::unique_ptr<IPC::Encoder>&);
+
+    void logInvalidMessage(IPC::Connection&, IPC::MessageName);
+
+    virtual void getLaunchOptions(ProcessLauncher::LaunchOptions&);
+
+    struct PendingMessage {
+        std::unique_ptr<IPC::Encoder> encoder;
+        OptionSet<IPC::SendOption> sendOptions;
+        Optional<std::pair<CompletionHandler<void(IPC::Decoder*)>, uint64_t>> asyncReplyInfo;
+    };
+
+    virtual bool shouldSendPendingMessage(const PendingMessage&) { return true; }
+
+    void didClose(IPC::Connection&);
+    void didReceiveInvalidMessage(IPC::Connection&, IPC::MessageName);
 
 private:
-    struct pcfetcher* m_fetcher;
-    IPC::Connection::Identifier m_identifier;
+    virtual void connectionWillOpen(IPC::Connection&);
 
-    RefPtr<PurCFetcher::ProcessLauncher> m_processLauncher;
+    struct pcfetcher* m_fetcher;
+
+    Vector<PendingMessage> m_pendingMessages;
+    RefPtr<ProcessLauncher> m_processLauncher;
+    RefPtr<IPC::Connection> m_connection;
+    IPC::MessageReceiverMap m_messageReceiverMap;
+    bool m_alwaysRunsAtBackgroundPriority { false };
+    PurCFetcher::ProcessIdentifier m_processIdentifier { PurCFetcher::ProcessIdentifier::generate() };
 };
+
+template<typename T>
+bool PcFetcherProcess::send(T&& message, uint64_t destinationID, OptionSet<IPC::SendOption> sendOptions)
+{
+    COMPILE_ASSERT(!T::isSync, AsyncMessageExpected);
+
+    auto encoder = makeUnique<IPC::Encoder>(T::name(), destinationID);
+    encoder->encode(message.arguments());
+
+    return sendMessage(WTFMove(encoder), sendOptions);
+}
+
+template<typename U>
+bool PcFetcherProcess::sendSync(U&& message, typename U::Reply&& reply, uint64_t destinationID, Seconds timeout, OptionSet<IPC::SendSyncOption> sendSyncOptions)
+{
+    COMPILE_ASSERT(U::isSync, SyncMessageExpected);
+
+    if (!m_connection)
+        return false;
+
+    TraceScope scope(SyncMessageStart, SyncMessageEnd);
+
+    return connection()->sendSync(std::forward<U>(message), WTFMove(reply), destinationID, timeout, sendSyncOptions);
+}
+
+template<typename T, typename C>
+void PcFetcherProcess::sendWithAsyncReply(T&& message, C&& completionHandler, uint64_t destinationID, OptionSet<IPC::SendOption> sendOptions, ShouldStartProcessThrottlerActivity shouldStartProcessThrottlerActivity)
+{
+    COMPILE_ASSERT(!T::isSync, AsyncMessageExpected);
+
+    auto encoder = makeUnique<IPC::Encoder>(T::name(), destinationID);
+    uint64_t listenerID = IPC::nextAsyncReplyHandlerID();
+    encoder->encode(listenerID);
+    encoder->encode(message.arguments());
+    sendMessage(WTFMove(encoder), sendOptions, {{ [completionHandler = WTFMove(completionHandler)] (IPC::Decoder* decoder) mutable {
+        if (decoder && decoder->isValid())
+            T::callReply(*decoder, WTFMove(completionHandler));
+        else
+            T::cancelReply(WTFMove(completionHandler));
+    }, listenerID }}, shouldStartProcessThrottlerActivity);
+}
 
 
 #endif // ENABLE(LINK_PURC_FETCHER)
 
 #endif /* not defined PURC_FETCHER_PROCESS_H */
-
-

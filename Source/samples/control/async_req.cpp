b73a0724fbc26deb2ea34d081ff3aae44e8b08f6
xueshuming <xueshuming@minigui.org>
2021-11-20 17:00:14 +0800

    copy sync_req.cpp asyn_req.cpp

---
+++ Source/samples/control/async_req.cpp
@@ -0,0 +1,62 @@
+
+#include "capi/fetcher.h"
+
+#include <wtf/RunLoop.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    const char* def_url = "https://hybridos.fmsoft.cn";
+    purc_instance_extra_info info = {0, 0};
+    purc_init ("cn.fmsoft.hybridos.sample", "pcfetcher", &info);
+
+    RunLoop::initializeMain();
+    AtomString::init();
+    WTF::RefCountedBase::enableThreadingChecksGlobally();
+
+    const char* url = argv[1] ? argv[1] : def_url;
+
+    pcfetcher_init(10, 1024);
+    struct pcfetcher_resp_header resp_header;
+    purc_rwstream_t resp = pcfetcher_request_sync(
+        url,
+        PCFETCHER_REQUEST_METHOD_GET,
+        NULL,
+        10,
+        &resp_header);
+
+    fprintf(stderr, "....................................\n");
+    fprintf(stderr, "%s\n", url);
+    fprintf(stderr, ".................head begin\n");
+    fprintf(stderr, "ret_code=%d\n", resp_header.ret_code);
+    fprintf(stderr, "mime_type=%s\n", resp_header.mime_type);
+    fprintf(stderr, "sz_resp=%ld\n", resp_header.sz_resp);
+    fprintf(stderr, ".................head end\n");
+    fprintf(stderr, ".................body begin\n");
+    if (resp) {
+        size_t sz_content = 0;
+        size_t sz_buffer = 0;
+        char* buf = (char*)purc_rwstream_get_mem_buffer_ex(resp, &sz_content,
+                &sz_buffer, false);
+        fprintf(stderr, "buffer size=%ld\n", sz_buffer);
+        fprintf(stderr, "body size=%ld|buflen=%ld\n", sz_content,
+                buf ? strlen(buf) : 0);
+        fprintf(stderr, "%s\n", buf ? buf : NULL);
+    }
+    fprintf(stderr, ".................body end\n");
+    fprintf(stderr, "....................................\n");
+
+//    RunLoop::run();
+
+    pcfetcher_term();
+    purc_cleanup();
+
+
+    return 0;
+}

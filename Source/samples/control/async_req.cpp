243694a8d0499570d52f6355dc956d850de9c072
xueshuming <xueshuming@minigui.org>
2021-11-20 17:12:21 +0800

    impl async_req

--- Source/samples/control/async_req.cpp
+++ Source/samples/control/async_req.cpp
@@ -7,36 +7,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char** argv)
+const char* def_url = "https://hybridos.fmsoft.cn";
+const char* url = NULL;
+
+void async_response_handler(
+        purc_variant_t request_id, void* ctxt,
+        const struct pcfetcher_resp_header *resp_header,
+        purc_rwstream_t resp)
 {
-    (void)argc;
-    (void)argv;
-
-    const char* def_url = "https://hybridos.fmsoft.cn";
-    purc_instance_extra_info info = {0, 0};
-    purc_init ("cn.fmsoft.hybridos.sample", "pcfetcher", &info);
-
-    RunLoop::initializeMain();
-    AtomString::init();
-    WTF::RefCountedBase::enableThreadingChecksGlobally();
-
-    const char* url = argv[1] ? argv[1] : def_url;
-
-    pcfetcher_init(10, 1024);
-    struct pcfetcher_resp_header resp_header;
-    purc_rwstream_t resp = pcfetcher_request_sync(
-        url,
-        PCFETCHER_REQUEST_METHOD_GET,
-        NULL,
-        10,
-        &resp_header);
-
+    UNUSED_PARAM(request_id);
+    UNUSED_PARAM(ctxt);
+    UNUSED_PARAM(resp);
     fprintf(stderr, "....................................\n");
     fprintf(stderr, "%s\n", url);
     fprintf(stderr, ".................head begin\n");
-    fprintf(stderr, "ret_code=%d\n", resp_header.ret_code);
-    fprintf(stderr, "mime_type=%s\n", resp_header.mime_type);
-    fprintf(stderr, "sz_resp=%ld\n", resp_header.sz_resp);
+    fprintf(stderr, "ret_code=%d\n", resp_header->ret_code);
+    fprintf(stderr, "mime_type=%s\n", resp_header->mime_type);
+    fprintf(stderr, "sz_resp=%ld\n", resp_header->sz_resp);
     fprintf(stderr, ".................head end\n");
     fprintf(stderr, ".................body begin\n");
     if (resp) {
@@ -51,12 +38,39 @@ int main(int argc, char** argv)
     }
     fprintf(stderr, ".................body end\n");
     fprintf(stderr, "....................................\n");
+    RunLoop::main().stop();
+}
 
-//    RunLoop::run();
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    purc_instance_extra_info info = {0, 0};
+    purc_init ("cn.fmsoft.hybridos.sample", "pcfetcher", &info);
+
+    RunLoop::initializeMain();
+    AtomString::init();
+    WTF::RefCountedBase::enableThreadingChecksGlobally();
+
+    url = argv[1] ? argv[1] : def_url;
+
+    pcfetcher_init(10, 1024);
+    pcfetcher_request_async(
+                url,
+                PCFETCHER_REQUEST_METHOD_GET,
+                NULL,
+                0,
+                async_response_handler,
+                NULL);
+
+    RunLoop::run();
+    fprintf(stderr, "....................................after runloop\n");
 
     pcfetcher_term();
     purc_cleanup();
 
-
     return 0;
 }
+
